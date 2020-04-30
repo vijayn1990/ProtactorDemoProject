072966c2981fdd02e7a0823d018eb77c668eb689
vijayn1990 <vijay.nagaraj2007@gmail.com>
2020-04-30 13:26:41 +0530

    Made changes to Calculator.js file

--- tests/calculator.js
+++ tests/calculator.js
@@ -9,72 +9,21 @@ describe('demo calculator tests', function(){
         
         //To slow execution
         browser.sleep(2000);
-
         homePage.enterFirstNumber('2');
-
         //To slow execution
         browser.sleep(2000);
 
         //element(by.model('second')).sendKeys('5');
         homePage.secondNumber('4');
-
         //To slow execution
         browser.sleep(2000);
-
-
         //element(by.css('[ng-click="doAddition()"]')).click();
         homePage.clickGo();
-
         browser.sleep(2000);
-
         //let res= element(by.cssContainingText('.ng-binding','7'));
-
         //console.log(expect(res.getText()).toEqual('7'));
-
         homePage.verifyResult('6');
-        
         browser.sleep(2000);
-
-
-
-    });
-
-    it('addition test', function(){
-        
-        //to launch and navigate to the URL
-        //browser.get('https://juliemr.github.io/protractor-demo/');
-        homePage.get('https://juliemr.github.io/protractor-demo/');
-        
-        //To slow execution
-        browser.sleep(2000);
-
-        homePage.enterFirstNumber('2');
-
-        //To slow execution
-        browser.sleep(2000);
-
-        //element(by.model('second')).sendKeys('5');
-        homePage.secondNumber('4');
-
-        //To slow execution
-        browser.sleep1(2000);
-
-
-        //element(by.css('[ng-click="doAddition()"]')).click();
-        homePage.clickGo();
-
-        browser.sleep(2000);
-
-        //let res= element(by.cssContainingText('.ng-binding','7'));
-
-        //console.log(expect(res.getText()).toEqual('7'));
-
-        homePage.verifyResult('6');
-        
-        browser.sleep(2000);
-
-
-
     });
 
 });
